9458b95bb170eb240a0b3b875b86001023103dbf
nohz.afk <149959021+nohzafk@users.noreply.github.com>
2024-07-29 14:02:20 +0800

    cache tools installed by install-deps.sh

--- .devcontainer/install-deps.sh
+++ .devcontainer/install-deps.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-npm install -g @devcontainers/cli
-npm install -g just-install
-pipx install cookiecutter
-pipx inject cookiecutter requests
+if ! command -v devcontainer; then
+    npm install -g @devcontainers/cli
+fi
+if ! command -v just; then
+    npm install -g just-install
+fi
+if [[ ! -f /usr/local/py-utils/venvs/cookiecutter/bin/cookiecutter ]]; then
+    pipx install cookiecutter
+    # pipx inject cookiecutter requests
+fi
--- justfile
+++ justfile
@@ -1,7 +1,13 @@
 set shell := ["bash", "-c"]
 
+# update all features
 generate:
     cd _generator && ./run_generator.sh
 
+# test an example feature
 test: generate
     cd _generator && ./test.sh basedpyright && ./test.sh typescript_eslint
+
+# open a devcontainer in VSCode
+devcontainer:
+    p=$(printf "%s" "$PWD" | hexdump -v -e '/1 "%02x"') && code --folder-uri "vscode-remote://dev-container+${p}/workspaces/$(basename $PWD)"
